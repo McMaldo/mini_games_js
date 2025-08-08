0af4c04f83a6cbaf3e3c6bff3731a5630d891250
McMaldo <maldonado.ignacio.pablo@gmail.com>
2025-08-08 12:33:26 -0300

    FEAT #002 * home page styles * star wars font * minecraft font

--- src/main.js
+++ src/main.js
@@ -11,8 +11,8 @@ let gameList = games.map(game => /*html*/`
   <article class="game-item">
     <h3>${game.name}</h3>
     <a href="./game/${game.url}">
-      View Demo
-      <img src="./public/icon/arrow.svg" alt="open" />
+      <img src="./public/icon/arrow.png" alt="" />
+      Play
     </a>
     <img class="portrait" src="./public/img/${game.url}.png" alt="${game.name} image" />
   </article>`
